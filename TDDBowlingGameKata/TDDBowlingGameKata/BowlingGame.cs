67be76d2348ab88f09710c44241bd34756f6a75d
Goedebuur <Arjen.Goedegebure@gmail.com>
2014-09-15 10:19:42 +0200

    Assignment 1 - Score gutter game correctly

---
+++ TDDBowlingGameKata/TDDBowlingGameKata/BowlingGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDBowlingGameKata
+{
+    public class BowlingGame
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public void Roll(int i)
+        {
+            
+        }
+
+        public int CalculateScore()
+        {
+            return 0;
+        }
+    }
+}
---
+++ TDDBowlingGameKata/TDDBowlingGameKataTests/BowlingGameTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using TDDBowlingGameKata;
+
+namespace TDDBowlingGameKataTests
+{
+    [TestFixture]
+    public class BowlingGameTests
+    {
+        [Test]
+        public void CalculateScore_GutterGame_ReturnsZero()
+        {
+            //Arrange
+            BowlingGame game = CreateBowlingGame();
+
+            //Act
+            for (int i = 0;
+                i < 20;
+                i++)
+            {
+                game.Roll(0);
+            }
+
+            //Assert
+            Assert.AreEqual(0, game.CalculateScore());
+        }
+
+        private BowlingGame CreateBowlingGame()
+        {
+            return new BowlingGame();
+        }
+    }
+}
